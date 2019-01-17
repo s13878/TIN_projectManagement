e9867b87de675f608253c37706dd76a06eb17768
jakubik <jakubik@DABROWD4-L>
2019-01-17 16:06:33 +0100

    create ProjectSummary module

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,33 +1,12 @@
 import React from 'react'
+import ProjectSummary from './ProjectSummary'
 
 const Projectlist = () => {
   return (
     <div className="project-list section">
-
-      <div className="card z-depth-0 project-sumary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Postet by the Omnioni</p>
-          <p className="grey-text">3rd September, 2am</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 project-sumary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Postet by the Omnioni</p>
-          <p className="grey-text">3rd September, 2am</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 project-sumary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Postet by the Omnioni</p>
-          <p className="grey-text">3rd September, 2am</p>
-        </div>
-      </div>
-
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
     </div>
 
   )
