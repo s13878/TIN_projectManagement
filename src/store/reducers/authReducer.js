891ee827081f06c33a20c4df81cee2f473efea1f
jakubik <jakubik@DABROWD4-L>
2019-01-23 23:21:27 +0100

    User SignUp (Firebase Auth)

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -6,19 +6,31 @@ const authReducer = (state = initState, action) => {
     switch(action.type){
         case 'LOGIN_ERROR':
         console.log('login error')
-        return{
-            ...state,
-            authError: 'Login failed'
-        }
+            return{
+                ...state,
+                authError: 'Login failed'
+            }
         case 'LOGIN_SUCCESS':
         console.log('login success')
-        return{
-            ...state,
-            authError: null
-        }
+            return{
+                ...state,
+                authError: null
+            }
         case 'LOGOUT_SUCCESS':
             console.log('logout success')
             return state;
+        case 'SIGNUP_SUCCESS':
+            console.log('signup success')
+            return{
+                ...state,
+                authError: null
+            }
+        case 'SIGNUP_ERROR':
+        console.log('signup error')
+            return{
+                ...state,
+                authError: action.err.message
+            }
         default:
             return state
     }
