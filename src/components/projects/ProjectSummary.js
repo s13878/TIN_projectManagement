9817ec9c89bd50cec12c92aa8fc3ae5b08326f5e
jakubik <jakubik@DABROWD4-L>
2019-01-24 00:03:39 +0100

    Addidn projects (visited)

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -5,7 +5,7 @@ const ProjectSummary = ({project, key}) => {
     <div className="card z-depth-0 project-sumary">
       <div className="card-content grey-text text-darken-3">
         <span className="card-title">{project.title}</span>
-        <p>Postet by the Omnioni</p>
+        <p>Postet by {project.authorFirstName} {project.authorLastName}</p>
         <p className="grey-text">3rd September, 2am</p>
       </div>
     </div>
