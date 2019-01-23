df253965b3978b341f27720d9729a87853fa11cf
jakubik <jakubik@DABROWD4-L>
2019-01-23 20:48:10 +0100

    Loggin user In

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,6 +1,22 @@
-const initState = {}
+const initState = {
+    authError: null
+}
 
 const authReducer = (state = initState, action) => {
+    switch(action.type){
+        case 'LOGIN_ERROR':
+        console.log('login error')
+        return{
+            ...state,
+            authError: 'Login failed'
+        }
+        case 'LOGIN_SUCCESS':
+        console.log('login success')
+        return{
+            ...state,
+            authError: null
+        }
+    }
     return state
 }
 
