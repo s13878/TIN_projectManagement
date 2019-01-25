b324c371652cd57fba065dd0a7940410da440b4e
jakubik <jakubik@DABROWD4-L>
2019-01-25 08:59:33 +0100

    Build app ant move to dist + hosting

---
+++ dist/precache-manifest.7482f2115a151803bdc5b400b82033b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f79025e70b6e302816c5",
+    "url": "/static/js/main.f79025e7.chunk.js"
+  },
+  {
+    "revision": "0f204be67a12b3a2e00e",
+    "url": "/static/js/1.0f204be6.chunk.js"
+  },
+  {
+    "revision": "f79025e70b6e302816c5",
+    "url": "/static/css/main.506572e7.chunk.css"
+  },
+  {
+    "revision": "ff4929a3d787252cc0aa12e0f6e3a319",
+    "url": "/index.html"
+  }
+];
