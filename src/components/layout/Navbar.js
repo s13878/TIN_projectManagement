899194b08776baaa9c57ab04f29d68e3c2dc1f66
jakubik <jakubik@DABROWD4-L>
2019-02-01 12:19:24 +0100

    update responsive navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,24 +4,44 @@ import SignInLinks from './SignedInLinks'
 import SignOutLinks from './SignedOutLinks'
 import { connect } from 'react-redux'
 
+
+
 const Navbar = (props) => {
-	const { auth, profile } = props;
+	const { close, open, auth, profile } = props;
 	// console.log(auth);
 	const links = auth.uid ? <SignInLinks profile={profile} /> : <SignOutLinks />;
 	return (
-		<nav className="nav-wrapper grey darken-3">
-			<div className="container">
-        <Link to='/' className="brand-logo">MarioPlan</Link>
-        { links }
-      </div>
-		</nav>
+		<div>
+			<nav className="nav-wrapper grey darken-3">
+				<div className="container">
+					<button href="#" data-target="mobile-demo" className="sidenav-trigger btn-flat" onClick={open}><i className="material-icons icon-white">menu</i></button>
+					<Link to='/' className="brand-logo">MarioPlan</Link>
+					<div id='topNav'>
+						{ links }
+					</div>
+				</div>
+			</nav>
+			<div className='mobile mobile__menu' onClick={close}>
+				{links}
+			</div>
+		</div>
 	)
 }
+
+
+
 const mapStateToProps = (state) => {
 	// console.log(state)
 	return {
 		auth: state.firebase.auth,
-		profile: state.firebase.profile
+		profile: state.firebase.profile,
+		open: ()=>{
+			var mobileNav = document.querySelector('.mobile');
+			mobileNav.classList.add('open');
+		},
+		close: (e)=>{
+			e.target.classList.remove('open');
+		}
 	}
 }
 
